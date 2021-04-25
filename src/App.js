9bee1ca0daa5171da80d7cd9ba706d066de9edfe
Brian Crisp <bcrisp084@yahoo.com>
2021-04-25 16:47:36 -0400

    updated app to direct it to the about me page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import About from './components/about';
+import {HashRouter as Router,Route }  from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+       <Route exact path ='/' component={About} />
     </div>
+    </Router>
   );
 }
 
